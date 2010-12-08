dfdd8f3d35ccd8ffbfa3eb10bbaa3c378691aca8
Cryolite <cryolite.indigo@gmail.com>
2010-12-08 16:34:44 +0900

    Fixed bugs in `CRADLE_PP_VA_REFUSE_EMPTY'. Added assertion for `CRADLE_PP_VA_SIZE'.

--- cradle/preprocessor/va/refuse_empty.hpp
+++ cradle/preprocessor/va/refuse_empty.hpp
@@ -5,13 +5,14 @@
 
 #include <boost/preprocessor/comparison/greater.hpp>
 #include <boost/preprocessor/control/iif.hpp>
+#include <boost/preprocessor/tuple/eat.hpp>
 #include <boost/preprocessor/facilities/expand.hpp>
 #include <boost/preprocessor/debug/assert.hpp>
 
 
 #define CRADLE_PP_VA_REFUSE_EMPTY_IMPL_F BOOST_PP_ASSERT(0)
 
-#define CRADLE_PP_VA_REFUSE_EMPTY_IMPL_E(...)
+#define CRADLE_PP_VA_REFUSE_EMPTY_IMPL_E(VA_TUPLE)
 
 #define CRADLE_PP_VA_REFUSE_EMPTY_IMPL_D(...)    \
 (__VA_ARGS__ ## F)                               \
@@ -21,19 +22,20 @@
 (CRADLE_PP_VA_REFUSE_EMPTY_IMPL_ ## __VA_ARGS__) \
 //////////////////////////////////////////////////
 
-#define CRADLE_PP_VA_REFUSE_EMPTY_IMPL_B(VA_TUPLE)  \
-BOOST_PP_EXPAND(                                    \
-  CRADLE_PP_VA_REFUSE_EMPTY_IMPL_E                  \
-    BOOST_PP_EXPAND(                                \
-      CRADLE_PP_VA_REFUSE_EMPTY_IMPL_D              \
-        CRADLE_PP_VA_REFUSE_EMPTY_IMPL_C VA_TUPLE)) \
-/////////////////////////////////////////////////////
+#define CRADLE_PP_VA_REFUSE_EMPTY_IMPL_B(VA_TUPLE) \
+CRADLE_PP_VA_REFUSE_EMPTY_IMPL_E(                  \
+  BOOST_PP_EXPAND(                                 \
+    CRADLE_PP_VA_REFUSE_EMPTY_IMPL_D               \
+      CRADLE_PP_VA_REFUSE_EMPTY_IMPL_C VA_TUPLE))) \
+////////////////////////////////////////////////////
 
-#define CRADLE_PP_VA_REFUSE_EMPTY_IMPL_A(SIZE, VA_TUPLE) \
-BOOST_PP_IIF(BOOST_PP_GREATER(SIZE, 1),                  \
-             CRADLE_PP_VA_REFUSE_EMPTY_IMPL_E,           \
-             CRADLE_PP_VA_REFUSE_EMPTY_IMPL_B)(VA_TUPLE) \
-//////////////////////////////////////////////////////////
+#define CRADLE_PP_VA_REFUSE_EMPTY_IMPL_A(SIZE, VA_TUPLE)       \
+BOOST_PP_TUPLE_EAT(1)(                                         \
+  BOOST_PP_EXPAND(xxx                                          \
+    BOOST_PP_IIF(BOOST_PP_GREATER(SIZE, 1),                    \
+                 CRADLE_PP_VA_REFUSE_EMPTY_IMPL_E,             \
+                 CRADLE_PP_VA_REFUSE_EMPTY_IMPL_B)(VA_TUPLE))) \
+////////////////////////////////////////////////////////////////
 
 #define CRADLE_PP_VA_REFUSE_EMPTY(...)                                \
 CRADLE_PP_VA_REFUSE_EMPTY_IMPL_A(CRADLE_PP_VA_SIZE_IMPL(__VA_ARGS__), \
