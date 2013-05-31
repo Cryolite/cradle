ecf6c26fec78e87909e7f924bb70a9a98057e642
Cryolite <cryolite.indigo@gmail.com>
2013-05-31 11:30:28 +0900

    Began support of XML-formatted logs for unit tests.
    * jamroot:
      * Added `<cradle-xunit-log-format>` and `<cradle-xunit-log-format-run>`
        features.
      * Added `CRADLE_XUNIT_LOG_FORMAT_RUN_PROPERTIES` hook.
      * Added `CRADLE_XUNIT_LOG_FORMAT_RUN_PROPERTIES` to
        `CRADLE_RUN_PROPERTIES` hook.
    * test/unit/exception/jamfile: Tweaked comments.
    * test/unit/exception/uncaught_exception_run.cpp: Added a global fixture to
      redirect the XML-formatted log output.

--- test/unit/exception/uncaught_exception_run.cpp
+++ test/unit/exception/uncaught_exception_run.cpp
@@ -4,9 +4,53 @@
 
 #define BOOST_TEST_MAIN
 #include <cradle/exception/uncaught_exception.hpp>
-#include <boost/test/unit_test.hpp>
-#include <cstddef>
 
+#include <boost/test/unit_test.hpp>
+
+#include <cstddef>
+#include <stdexcept>
+#include <memory>
+#include <string>
+#include <fstream>
+
+class GlobalFixture
+{
+public:
+  GlobalFixture()
+    : p_log_stream_()
+  {
+    std::size_t const argc = boost::unit_test::framework::master_test_suite().argc;
+    char const * const * const argv = boost::unit_test::framework::master_test_suite().argv;
+    if (argc != 2u) {
+      throw std::runtime_error("");
+    }
+    if (argv[1u] == std::string("--xunit-log-format=hrf")) {
+      // Do nothing.
+    }
+    else if (argv[1u] == std::string("--xunit-log-format=xml")) {
+      std::string log_file_path(argv[0u]);
+      log_file_path += ".xml" ;
+      p_log_stream_.reset(new std::ofstream(log_file_path.c_str()));
+      boost::unit_test::unit_test_log.set_stream(*p_log_stream_);
+    }
+    else {
+      throw std::runtime_error("");
+    }
+  }
+
+  ~GlobalFixture()
+  {
+    if (p_log_stream_) {
+      *p_log_stream_ << "</TestLog>" << std::flush;
+      boost::unit_test::unit_test_log.set_stream(std::cout);
+    }
+  }
+
+private:
+  std::shared_ptr<std::ostream> p_log_stream_;
+};
+
+BOOST_GLOBAL_FIXTURE(GlobalFixture)
 
 BOOST_AUTO_TEST_CASE(testZero)
 {
