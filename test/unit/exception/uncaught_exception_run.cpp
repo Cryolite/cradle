fae6ba0a96a3876a2528efbf8867e5b142078a67
Cryolite <cryolite.indigo@gmail.com>
2013-05-31 16:15:31 +0900

    Added support of module-based test organization for `functional`.
    * test/unit/jamfile: Added `master` target.
    * test/unit/master.cpp: Newly added.
    * test/unit/exception/jamfile: Removed `compile`, `compile-fail`, `run` and
      `run-fail` targets.
    * test/unit/exception/uncaught_exception_run.cpp:
      * Moved the global fixture to `test/unit/master.cpp`.
      * Tweaked the names of the test cases.
    * test/unit/functional/invoke_run.cpp: Tweaked the names of the test cases.
    * test/unit/functional/jamfile: Removed `compile`, `compile-fail`, `run` and
      `run-fail` targets.

--- test/unit/exception/uncaught_exception_run.cpp
+++ test/unit/exception/uncaught_exception_run.cpp
@@ -2,66 +2,21 @@
 //  Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#define BOOST_TEST_MAIN
 #include <cradle/exception/uncaught_exception.hpp>
-
 #include <boost/test/unit_test.hpp>
-
 #include <cstddef>
-#include <stdexcept>
-#include <memory>
-#include <string>
-#include <iostream>
-#include <fstream>
 
-class GlobalFixture
-{
-public:
-  GlobalFixture()
-    : p_log_stream_()
-  {
-    std::size_t const argc
-      = boost::unit_test::framework::master_test_suite().argc;
-    char const * const * const argv
-      = boost::unit_test::framework::master_test_suite().argv;
-    if (argc != 2u) {
-      throw std::runtime_error(
-        "an invalid number of arguments passed to the unit test module");
-    }
-    if (argv[1u] == std::string("--xunit-log-format=hrf")) {
-      // Do nothing.
-    }
-    else if (argv[1u] == std::string("--xunit-log-format=xml")) {
-      std::string log_file_path(argv[0u]);
-      log_file_path += ".xml" ;
-      p_log_stream_.reset(new std::ofstream(log_file_path.c_str()));
-      boost::unit_test::unit_test_log.set_stream(*p_log_stream_);
-    }
-    else {
-      throw std::runtime_error("");
-    }
-  }
 
-  ~GlobalFixture()
-  {
-    if (p_log_stream_) {
-      *p_log_stream_ << "</TestLog>" << std::flush;
-      boost::unit_test::unit_test_log.set_stream(std::cout);
-    }
-  }
+BOOST_AUTO_TEST_SUITE(exception)
 
-private:
-  std::shared_ptr<std::ostream> p_log_stream_;
-};
+BOOST_AUTO_TEST_SUITE(uncaught_exception)
 
-BOOST_GLOBAL_FIXTURE(GlobalFixture)
-
-BOOST_AUTO_TEST_CASE(testZero)
+BOOST_AUTO_TEST_CASE(zero)
 {
   BOOST_CHECK_EQUAL(cradle::exceptions::get_num_uncaught_exceptions(), 0u);
 }
 
-BOOST_AUTO_TEST_CASE(testCatch)
+BOOST_AUTO_TEST_CASE(catch_)
 {
   try {
     throw 0;
@@ -103,9 +58,9 @@ private:
   std::size_t n_;
 };
 
-}
+} // namespace `unnamed'
 
-BOOST_AUTO_TEST_CASE(testUnwind)
+BOOST_AUTO_TEST_CASE(unwind)
 {
   BOOST_CHECK_EQUAL(cradle::exceptions::get_num_uncaught_exceptions(), 0u);
   try {
@@ -119,3 +74,7 @@ BOOST_AUTO_TEST_CASE(testUnwind)
   }
   BOOST_CHECK_EQUAL(cradle::exceptions::get_num_uncaught_exceptions(), 0u);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE_END()
